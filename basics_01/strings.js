7f2f950a112068cee65639f00c5c2f1729d8d2f6
Ayushkumar302 <ayushxkumar2002@gmail.com>
2024-04-11 11:11:47 +0530

    strings

---
+++ basics_01/strings.js
@@ -0,0 +1,24 @@
+// strings in js  
+// a string is a sequence of characters used to represent text. Strings are commonly used for storing and manipulating textual 
+//data in computer programs. They can be manipulated using various operations like concatenation, substring extraction, and comparison.
+// string literals - single quotes or double quotes 
+// let name = 'John';
+
+let n = "john";
+// let n = 'john';
+console.log(n);
+console.log(n.length);
+console.log(n[0]);
+console.log(n[1]);
+
+// template literals
+let boy1 = "John";
+let boy2 = "Jake";
+let sentence = `${boy1} is a friend of ${boy2}`;// interpolation 
+console.log(sentence);
+
+//escape sequence characters
+let fruit = 'bana\'na'//  escaping the quote so it doesn't   , same for double quotes
+console.log(fruit)
+
+
