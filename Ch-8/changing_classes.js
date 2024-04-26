2961fe9bf8c349d924fd9776dd2c6c56fd0f8c8b
Ayushkumar302 <ayushxkumar2002@gmail.com>
2024-04-26 13:40:34 +0530

    changing_classes

---
+++ Ch-8/changing_classes.js
@@ -0,0 +1,8 @@
+Fst.className = "text-dark yellow"
+
+// Fst.classList
+// Fst.classList.remove("yellow")
+// Fst.classList.add("red")
+// Fst.classList.toggle("red")
+// Fst.classList.contains("red")
+
