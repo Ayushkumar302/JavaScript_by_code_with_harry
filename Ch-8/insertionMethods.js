57f7b0b875f8cdbce63f08afb9ea02a2897d0e87
Ayushkumar302 <ayushxkumar2002@gmail.com>
2024-04-26 13:22:26 +0530

    INSERT ADJACENT methods

--- Ch-8/insertionMethods.js
+++ Ch-8/insertionMethods.js
@@ -13,3 +13,14 @@ div.innerHTML =  '<h3>hello world!!!!</h3>'
 // a.after(div)
 // a.replaceWith(div)
 
+//**********INSERT ADJACENT methods */
+//1. insertAdjacentHTML
+//2. insertAdjacentElement
+//3. insertAdjacentText
+
+containerTwo.insertAdjacentHTML('beforebegin', '<div class="test">beforebegin</div>')
+containerTwo.insertAdjacentHTML('beforeend', '<div class="test">beforend</div>')
+containerTwo.insertAdjacentHTML('afterbegin', '<div class="test">afterbegin</div>')
+containerTwo.insertAdjacentHTML('afterend', '<div class="test">afterend</div>')
+
+
