ebc182a421e7ca0edd415aa309371207c6022b4f
Ayushkumar302 <ayushxkumar2002@gmail.com>
2024-04-27 12:09:13 +0530

    practice set

---
+++ practice_set8.js
@@ -0,0 +1,9 @@
+document.getElementById("google").addEventListener("click",function () {
+    window.location = "https://www.google.com/";
+})
+document.getElementById("facebook").addEventListener("click",function () {
+    window.location = "https://www.facebook.com";
+})
+document.getElementById("youtube").addEventListener("click",function () {
+    window.location = "https://www.youtube.com";
+})
