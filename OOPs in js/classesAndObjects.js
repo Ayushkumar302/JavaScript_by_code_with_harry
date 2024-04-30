5aef0f05c4f52f67764e466c2d4a03da41be1bfc
Ayushkumar302 <ayushxkumar2002@gmail.com>
2024-04-30 12:48:17 +0530

    updated

--- OOPs in js/classesAndObjects.js
+++ OOPs in js/classesAndObjects.js
@@ -7,20 +7,43 @@
 // We can create multiple objects from a class.
 // Objects are created at runtime.
 
-class RailwayForm{
-    submit(){
-        alert("form submitted")
-    }
-    cancel() {
-        alert("form cancelled")
-    }
+// class RailwayForm{
+//     submit(){
+//         alert("form submitted")
+//     }
+//     cancel() {
+//         alert("form cancelled")
+//     }
+// }
+
+// let john = new RailwayForm()
+// john.submit()
+
+// // john.cancel()
+
+// let jane = new RailwayForm()
+// jane.submit()
+// jane.cancel()
+
+class RailwayForm {
+  submit() {
+    alert(`${this.name} your form is submitted for train number ${this.trainNo}`);
+  }
+  cancel() {
+    alert(`${this.name} your form is canceled for train number ${this.trainNo}`);
+  }
+  fill(givenname , trainNo) {
+    this.name = givenname;
+    this.trainNo = trainNo;
+  }
 }
 
-let john = new RailwayForm()
-john.submit()
+let johnForm = new RailwayForm();
+johnForm.fill("John",12142)
 
-// john.cancel()
+let janeForm = new RailwayForm();
+janeForm.fill("Jane",14333)
 
-let jane = new RailwayForm()
-jane.submit()
-jane.cancel()
+johnForm.submit();
+janeForm.submit();
+janeForm.cancel();
